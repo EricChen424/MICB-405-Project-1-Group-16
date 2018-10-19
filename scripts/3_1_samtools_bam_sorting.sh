8c1bd67f05c14e9c5bf9303cea0f3a468117c334
Eric Chen <l0z9a@ugrad.cs.ubc.ca>
2018-10-19 14:04:11 -0700

    for loops

--- scripts/3_1_samtools_bam_sorting.sh
+++ scripts/3_1_samtools_bam_sorting.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
 #TODO: consider doing other steps here like rmdup, filtering out certain bit sequences in view
-samtools sort ~/Project_1/2_BWA_Output/Cat.bam 1>~/Project_1/3_SamTools_Output/Cat.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Bat.bam 1>~/Project_1/3_SamTools_Output/Bat.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Guinea_pig.bam 1>~/Project_1/3_SamTools_Output/Guinea_pig.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Rabid_raccoon_1.bam 1>~/Project_1/3_SamTools_Output/Rabid_raccoon_1.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Rabid_raccoon_2.bam 1>~/Project_1/3_SamTools_Output/Rabid_raccoon_2.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Rabid_raccoon_3.bam 1>~/Project_1/3_SamTools_Output/Rabid_raccoon_3.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Rabid_raccoon_4.bam 1>~/Project_1/3_SamTools_Output/Rabid_raccoon_4.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_1.bam 1>~/Project_1/3_SamTools_Output/Patient_1.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_2.bam 1>~/Project_1/3_SamTools_Output/Patient_2.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_3.bam 1>~/Project_1/3_SamTools_Output/Patient_3.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_4.bam 1>~/Project_1/3_SamTools_Output/Patient_4.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_5.bam 1>~/Project_1/3_SamTools_Output/Patient_5.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_6.bam 1>~/Project_1/3_SamTools_Output/Patient_6.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_7.bam 1>~/Project_1/3_SamTools_Output/Patient_7.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_8.bam 1>~/Project_1/3_SamTools_Output/Patient_8.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_9.bam 1>~/Project_1/3_SamTools_Output/Patient_9.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_10.bam 1>~/Project_1/3_SamTools_Output/Patient_10.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_11.bam 1>~/Project_1/3_SamTools_Output/Patient_11.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_12.bam 1>~/Project_1/3_SamTools_Output/Patient_12.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_13.bam 1>~/Project_1/3_SamTools_Output/Patient_13.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_14.bam 1>~/Project_1/3_SamTools_Output/Patient_14.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_15.bam 1>~/Project_1/3_SamTools_Output/Patient_15.sorted.bam
-samtools sort ~/Project_1/2_BWA_Output/Patient_16.bam 1>~/Project_1/3_SamTools_Output/Patient_16.sorted.bam
+for bam in ~/Project_1/2_BWA_Output/*.bam;
+do
+	prefix=$(basename $bam | sed 's/.bam//g')
+	samtools sort ~/Project_1/2_BWA_Output/$prefix\.bam 1>~/Project_1/3_SamTools_Output/$prefix\.sorted.bam
+done
--- scripts/3_2_samtools_index.sh
+++ scripts/3_2_samtools_index.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
 #this step doesn't really have any worthwhile parameters
-samtools index ~/Project_1/3_SamTools_Output/Cat.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Bat.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Guinea_pig.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Rabid_raccoon_1.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Rabid_raccoon_2.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Rabid_raccoon_3.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Rabid_raccoon_4.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_1.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_2.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_3.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_4.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_5.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_6.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_7.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_8.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_9.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_10.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_11.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_12.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_13.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_14.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_15.sorted.bam
-samtools index ~/Project_1/3_SamTools_Output/Patient_16.sorted.bam
+for sortedbam in ~/Project_1/3_SamTools_Output/*.sorted.bam;
+do
+	prefix=$(basename $sortedbam | sed 's/.sorted.bam//g')
+	samtools index ~/Project_1/3_SamTools_Output/$prefix\.sorted.bam
+done
