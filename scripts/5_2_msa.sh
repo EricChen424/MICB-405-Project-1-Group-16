1a1c2368778647d75a821157dddb94102c6abe43
echen424 <l0z9a@ugrad.cs.ubc.ca>
2018-10-19 19:01:15 -0700

    Change MSA aligner and make tree script

--- scripts/5_2_msa.sh
+++ scripts/5_2_msa.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #TODO: parameters
-muscle -in ~/Project_1/4_BcfTools_Output/all_variants.fasta \
--out ~Project_1/5_MSA_Output/all_variants.mfa
+mafft --auto ~/Project_1/4_BcfTools_Output/all_variants.fasta \
+> ~Project_1/5_MSA_Output/all_variants.mfa
