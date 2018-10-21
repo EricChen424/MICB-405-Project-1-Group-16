eb6ab3601cb71c27b635f076ff5b307253aeecfa
echen424 <l0z9a@ugrad.cs.ubc.ca>
2018-10-20 23:44:02 -0700

    stats after rmdup

--- revised_scripts/3_0_samtools_flagstat.sh
+++ revised_scripts/3_0_samtools_flagstat.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-for bam in ~/Project_1/2_BWA_Output/*.bam;
+for bam in ~/Project_1/3_SamTools_Output/*.sorted.rmdup.bam;
 do
-	prefix=$(basename $bam | sed 's/.bam//g')
-	samtools flagstat ~/Project_1/2_BWA_Output/$prefix\.bam  > ~/Project_1/2_BWA_Output/stats/$prefix\.txt
+	prefix=$(basename $bam | sed 's/.sorted.rmdup.bam//g')
+	samtools flagstat ~/Project_1/3_SamTools_Output/$prefix\.sorted.rmdup.bam  > ~/Project_1/2_BWA_Output/stats/$prefix\.txt
 done
--- revised_scripts/3_2_samtools_bam_rmdup.sh
+++ revised_scripts/3_2_samtools_bam_rmdup.sh
@@ -3,5 +3,6 @@
 for sortedbam in ~/Project_1/3_SamTools_Output/*.sorted.bam;
 do
 	prefix=$(basename $sortedbam | sed 's/.sorted.bam//g')
-	samtools markdup -r -s ~/Project_1/3_SamTools_Output/$prefix\.sorted.rmdup.bam
+	samtools rmdup ~/Project_1/3_SamTools_Output/$prefix\.sorted.bam \
+	~/Project_1/3_SamTools_Output/$prefix\.sorted.rmdup.bam
 done
