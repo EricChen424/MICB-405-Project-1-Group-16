e28cc8dc1b5cda469dfb19766788aa682dda1116
echen424 <l0z9a@ugrad.cs.ubc.ca>
2018-10-21 22:47:33 -0700

    trees

--- revised_scripts/6_tree.sh
+++ revised_scripts/6_tree.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-#TODO: parameters
-raxml-ng --all --msa ~/Project_1/5_MSA_Output/all_variants_trimal.mfa \
---model LG+G2 --tree rand{4} --bs-trees 4 \
---threads 2 --seed 12345
+cp ~/Project_1/5_MSA_Output/filtered/all_variants_trimal.mfa ~/Project_1/6_Tree/filtered/all_variants_trimal.mfa
+cp ~/Project_1/5_MSA_Output/strict_filter/all_variants_trimal.mfa ~/Project_1/6_Tree/strict_filter/all_variants_trimal.mfa
+
+raxml-ng --all --msa ~/Project_1/6_Tree/filtered/all_variants_trimal.mfa \
+--model LG+G2 --tree rand{20} --bs-trees 100 \
+--threads 1 --seed 12345
+
+raxml-ng --all --msa ~/Project_1/6_Tree/strict_filter/all_variants_trimal.mfa \
+--model LG+G2 --tree rand{20} --bs-trees 100 \
+--threads 1 --seed 12345
