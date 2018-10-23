ed54a01adf5a3e91ac7f3a7699f8167d8a62596e
echen424 <l0z9a@ugrad.cs.ubc.ca>
2018-10-22 23:26:15 -0700

    master script

--- revised_scripts/3_2_samtools_bam_rmdup.sh
+++ revised_scripts/3_2_samtools_bam_rmdup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#TODO: consider doing other steps here like rmdup, filtering out certain bit sequences in view
 for sortedbam in ~/Project_1/3_SamTools_Output/*.sorted.bam;
 do
 	prefix=$(basename $sortedbam | sed 's/.sorted.bam//g')
--- revised_scripts/4_1_bcftools_variant_calling.sh
+++ revised_scripts/4_1_bcftools_variant_calling.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#filter out quality < 30 and indels to begin with
+#filter out indels to begin with
 for sortedbam in ~/Project_1/3_SamTools_Output/*.sorted.rmdup.bam;
 do
 	prefix=$(basename $sortedbam | sed 's/.sorted.rmdup.bam//g')
-	bcftools mpileup -q 30 -I --fasta-ref ~/Project_1/ref_genome.fasta \
+	bcftools mpileup -I --fasta-ref ~/Project_1/ref_genome.fasta \
 	~/Project_1/3_SamTools_Output/$prefix\.sorted.rmdup.bam | bcftools call -mv - \
 	>~/Project_1/4_BcfTools_Output/raw/$prefix\.raw.vcf
 done
