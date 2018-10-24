e7c51bcbb495bde19e3ae29f31e98fb6d991c705
echen424 <l0z9a@ugrad.cs.ubc.ca>
2018-10-23 20:38:52 -0700

    models

--- master_scripts/tree_script.sh
+++ master_scripts/tree_script.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 #6_tree
-rm ~/Project_1/6_Tree/filtered/*
-rm ~/Project_1/6_Tree/strict_filter/*
-cp ~/Project_1/5_MSA_Output/filtered/all_variants_trimal.mfa ~/Project_1/6_Tree/filtered/all_variants_trimal.mfa
-cp ~/Project_1/5_MSA_Output/strict_filter/all_variants_trimal.mfa ~/Project_1/6_Tree/strict_filter/all_variants_trimal.mfa
+rm -r ~/Project_1/6_Tree/*
+mkdir ~/Project_1/6_Tree/JC+G4
+mkdir ~/Project_1/6_Tree/JC+G4/filtered
+mkdir ~/Project_1/6_Tree/JC+G2
+mkdir ~/Project_1/6_Tree/JC+G2/filtered
+mkdir ~/Project_1/6_Tree/JC
+mkdir ~/Project_1/6_Tree/JC/filtered
 
-raxml-ng --all --msa ~/Project_1/6_Tree/filtered/all_variants_trimal.mfa \
---model GTR+G4 --tree rand{20} --bs-trees 250 \
+cp ~/Project_1/5_MSA_Output/filtered/all_variants_trimal.mfa ~/Project_1/6_Tree/JC+G4/filtered/all_variants_trimal.mfa
+cp ~/Project_1/5_MSA_Output/filtered/all_variants_trimal.mfa ~/Project_1/6_Tree/JC+G2/filtered/all_variants_trimal.mfa
+cp ~/Project_1/5_MSA_Output/filtered/all_variants_trimal.mfa ~/Project_1/6_Tree/JC/filtered/all_variants_trimal.mfa
+
+raxml-ng --all --msa ~/Project_1/6_Tree/JC+G4/filtered/all_variants_trimal.mfa \
+--model JC+G4 --tree rand{20} --bs-trees 250 \
 --threads 1 --seed 12345
 
-raxml-ng --all --msa ~/Project_1/6_Tree/strict_filter/all_variants_trimal.mfa \
---model GTR+G4 --tree rand{20} --bs-trees 250 \
+raxml-ng --all --msa ~/Project_1/6_Tree/JC+G2/filtered/all_variants_trimal.mfa \
+--model JC+G2 --tree rand{20} --bs-trees 250 \
+--threads 1 --seed 12345
+
+raxml-ng --all --msa ~/Project_1/6_Tree/JC/filtered/all_variants_trimal.mfa \
+--model JC --tree rand{20} --bs-trees 250 \
 --threads 1 --seed 12345
